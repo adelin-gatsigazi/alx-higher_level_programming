d97723a5c5ee95d5ef3d72a10f13e781ec1e43bf
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-01-10 23:06:58 +0200

    py brain

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,15 @@
-
 #!/usr/bin/python3
-"""Defines a class Student."""
+""" creating student class """
 
 
 class Student:
-    """Represent a student."""
-
+    """ Defining a class"""
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """ Initializing a class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of the Student."""
+        """ converting to json """
         return self.__dict__
