4e35bb2585d0fd8789d7eaea685b209e3d30d2b4
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-01-11 23:00:12 +0200

    py brain

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
 def print_stats(size, status_codes):
@@ -11,7 +16,6 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
-
 
 if __name__ == "__main__":
     import sys
