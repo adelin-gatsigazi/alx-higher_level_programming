b43438869d4e2045384d800cd0c0f40c61d5479f
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-04-14 02:05:06 +0200

    update concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const [arg1 = "No argument", arg2 = "No argument"] = process.argv.slice(2);
+const [arg1 = 'undefined', arg2 = 'undefined'] = process.argv.slice(2);
 console.log(`${arg1} is ${arg2}`);
