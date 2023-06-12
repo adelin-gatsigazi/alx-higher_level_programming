b4c9ab89e1a1c8b8948056de3e0805fea5865940
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-06-13 01:48:34 +0200

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arr1 = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i < arrS.length; i++) {
-  console.log(arrS[i]);
- }
+for (let i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+ } 
