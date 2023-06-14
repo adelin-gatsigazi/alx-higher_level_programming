5f6b87bbd0dbbdbadf402e42b802b3a08ff68bd2
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-06-14 22:24:14 +0200

    sq5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
+#!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
-  }
-  
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  
-  double() {
-    this.width *= 2;
-    this.height *= 2;
   }
 }
 
-module.exports = Square; 
+module.exports = Square;
