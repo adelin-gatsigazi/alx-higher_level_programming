d80a4c4a48b699adcb55d9e2b522cf7b435f2054
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-07-26 00:37:48 +0200

    js web

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const todos = JSON.parse(body);
-    const completedTasksByUser = {};
-
-    todos.forEach(todo => {
-      if (todo.completed) {
-        if (completedTasksByUser[todo.userId]) {
-          completedTasksByUser[todo.userId]++;
-        } else {
-          completedTasksByUser[todo.userId] = 1;
-        }
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-
-    console.log(completedTasksByUser);
+    console.log(completed);
   }
 });
