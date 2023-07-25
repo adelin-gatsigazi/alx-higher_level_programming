590c3ede7e33cbf1ba1c1fc8b9f9dbd551197a61
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-07-25 23:27:35 +0200

    description

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const fs = require('fs');
 
 const filePath = process.argv[2];
@@ -8,5 +9,4 @@ fs.readFile(filePath, 'utf-8', (error, data) => {
   } else {
     console.log(data);
   }
-});
-
+}); 
